1c172a27dc335c89c685ff6fac66062052d3c670
MrFogg866 <philiplynch866@gmail.com>
2022-11-09 13:03:01 +0000

    add secret key back

--- scottys/settings.py
+++ scottys/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-otwlkvga@cl7rnfky%o0vgmyumf=&&u^hmwzcvd23)h^+*&b63'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
