89cbcda4d99fe62f4aa68a59fde83a53a9ac7988
MrFogg866 <philiplynch866@gmail.com>
2022-12-04 13:05:40 +0000

    reorder the product list sequence in admin.py

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,26 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+
+    )
+
+    ordering =('sku',)
+
+class CatergoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'freindly_name',
+        'name',
+    )
+
+
 admin.site.register(Product)
 admin.site.register(Category)
