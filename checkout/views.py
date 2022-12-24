afb67fa999d72343268be46ac479fcebf9bddb01
MrFogg866 <philiplynch866@gmail.com>
2022-12-24 14:53:18 +0000

    add stripe payments to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key' : 'pk_live_51MGrOLETAIkYhLE8wWKlgWAOxMFcrUGiT9euwAwvCYTlSUYbrb50YfZt5STRHlzA9Jl8UQPnvaAZVVfmomyeRdhO00XTpz7IaV',
+        'client_secret' : 'test client secret',
     }
 
     return render(request, template, context)
