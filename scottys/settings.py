8ff87c14640eba7b4d01d7eff61690076903465d
MrFogg866 <philiplynch866@gmail.com>
2022-12-30 11:50:52 +0000

    removed secret key and set dubug

--- scottys/settings.py
+++ scottys/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True 
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'your-app.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'scotties.herokuapp.com']
 
 
 # Application definition
@@ -125,13 +125,17 @@ WSGI_APPLICATION = 'scottys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-     
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
