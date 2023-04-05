c1bddd736497c84853f77670995054892652266f
MrFogg866 <philiplynch866@gmail.com>
2023-04-05 19:46:36 +0000

    check database from project

--- scottys/settings.py
+++ scottys/settings.py
@@ -134,17 +134,12 @@ WSGI_APPLICATION = 'scottys.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
